5ec0ccbfd3978fb07ba9efbccb643007194f2c15
Rasmus Moorats <me@neonsea.uk>
2019-04-14 16:05:37 +0100

    Updated for blog post

---
+++ README.md
@@ -0,0 +1,19 @@
+# karch
+
+A KUAL extension to easily and automatically manage an Arch Linux chroot installation on an Amazon Kindle.
+
+### Requirements
+* A newer model rooted Kindle
+* [KUAL](https://www.mobileread.com/forums/showthread.php?t=203326)
+
+### Installation
+First, you need to create an Arch Linux filesystem and transfer it to your Kindle. Follow the steps detailed [in this post](https://neonsea.uk/blog/2019/04/14/chroot-shenanigans-2.html) up until you've created the filesystem and transferred it over. Everything else is suggested -- to get an understanding of how it works -- but not mandatory. 
+
+You can then clone or download this repository and extract it to your KUAL `extensions` directory on your Kindle.
+
+Place the filesystem (by default named `arch.img`) in the `karch` extension directory.
+
+### Usage
+Simply launch KUAL and select `karch` from the menu. You can then choose to launch the environment.
+
+If you have the [`kterm` KUAL extension](https://www.fabiszewski.net/kindle-terminal/), you will also have an option to launch the chroot in a terminal window instead of a graphical environment.
--- karch/karch.sh
+++ karch/karch.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 set -x
 SCREENSIZE=$(eips -i | grep 'xres:' | awk '{print $2"x"$4}')
+SERVICES="framework pillow webreader kb contentpackd"
 
 WORKDIR=$(pwd)
+CHROOT_DIRECTORY="system"
+FS_FILE="arch.img"
+
+CHRPATH="${WORKDIR}/${CHROOT_DIRECTORY}"
 
 INITSCRIPT="#!/bin/sh
-echo 'nameserver 8.8.8.8' > /etc/resolv.conf
+
+rm /etc/resolv.conf;
+echo 'nameserver 8.8.8.8' > /etc/resolv.conf;
 test -e /etc/ready || {
     pacman -S lxde xorg-server-xephyr --noconfirm;
     if [ $? -eq 0 ]; then
@@ -34,8 +41,8 @@ print_bot()
 
 check_host()
 {
-    test -e "$WORKDIR"/arch.img || {
-        print_bot "arch.img not found!" 0;
+    test -e "${WORK_DIR}/${FS_FILE}" || {
+        print_bot "${FS_FILE} not found!";
         exit 1;
     }
 }
@@ -43,58 +50,66 @@ check_host()
 mountall(){
     rm /dev/loop/karch;
     mknod -m0660 /dev/loop/karch b 7 250;
-    mkdir "$WORKDIR"/system/;
-    mount -o loop=/dev/loop/karch -t ext3 "$WORKDIR"/arch.img "$WORKDIR"/system/ ||
-      mount /dev/loop/karch -t ext3 "$WORKDIR"/system/;
-    mount -o bind /dev "$WORKDIR"/system/dev;
-    mount -o bind /dev/pts "$WORKDIR"/system/dev/pts;
-    mount -o bind /proc "$WORKDIR"/system/proc;
-    mount -o bind /sys "$WORKDIR"/system/sys;
-    cp /etc/hosts "$WORKDIR"/system/;
+    mkdir "${CHRPATH}";
+    mount -o loop=/dev/loop/karch -t ext4 "${WORK_DIR}/${FS_FILE}" "${CHRPATH}" ||
+      mount /dev/loop/karch -t ext4 "${CHRPATH}";
+    mount -o bind /dev     "${CHRPATH}/dev";
+    mount -o bind /dev/pts "${CHRPATH}/dev/pts";
+    mount -o bind /proc    "${CHRPATH}/proc";
+    mount -o bind /sys     "${CHRPATH}/sys";
+    mount -o bind /tmp     "${CHRPATH}/tmp";
+    cp /etc/hosts          "${CHRPATH}/";
 }
 
 umountall(){
-    umount "$WORKDIR"/system/sys;
-    umount "$WORKDIR"/system/proc;
-    umount "$WORKDIR"/system/dev/pts;
-    umount "$WORKDIR"/system/dev;
-    umount -lf "$WORKDIR"/system;
+    umount "${CHRPATH}/tmp";
+    umount "${CHRPATH}/sys";
+    umount "${CHRPATH}/proc";
+    umount "${CHRPATH}/dev/pts";
+    umount "${CHRPATH}/dev";
+    umount -lf "${CHRPATH}";
     losetup -d /dev/loop/karch; # TODO: Figure out why it doesn't work after quitting DE
-    rm -rf "$WORKDIR"/system;
+    rm -rf "${CHRPATH}";
 }
 
 launch(){
     check_host;
     trap umountall INT TERM;
     mountall;
-    chroot "$WORKDIR"/system/ /bin/bash;
+    chroot "${CHRPATH}" /bin/bash;
     umountall;
+}
+
+gui(){
+    for service in ${SERVICES}; do 
+        ${1} ${service}; 
+    done
 }
 
 main(){
     check_host;
     trap umountall INT TERM;
-    stop framework;
-    stop webreader;
+    gui stop;
     killall Xephyr;
     print_bot "Starting karch";
     mountall;
-    test -e "$WORKDIR"/system/usr/sbin/init-de.sh || {
-        echo "$INITSCRIPT" > "$WORKDIR"/system/usr/sbin/init-de.sh;
+    test -e "${CHRPATH}/usr/sbin/init-de.sh" || {
+        echo "${INITSCRIPT}" > "${CHRPATH}/usr/sbin/init-de.sh";
+        chmod +x "${CHRPATH}/usr/sbin/init-de.sh";
         print_bot "First boot may take a while";
     }
     trap "true" HUP INT TERM
-    chroot "$WORKDIR"/system/ /bin/bash /usr/sbin/init-de.sh;
+    chroot "${CHRPATH}" /bin/bash /usr/sbin/init-de.sh;
     killall Xephyr;
-    test ! -e "$WORKDIR"/system/etc/failed || {
+    test ! -e "${CHRPATH}/etc/failed" || {
         print_bot "Installing UI failed";
+        sleep 2;
     }
     print_bot "Restoring Kindle UI, please wait...";
     umountall;
     sleep 3;
     umountall;
-    start webreader;
-    start framework;
+    gui start;
 }
 
 if [ -z "$1" ]; then
